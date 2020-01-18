07cc3e1a7940e47774a429e45364c04339e02a3e
Anonymous <user@rivne.itstep.org>
2020-01-18 10:24:57 +0200

    add 4 methods

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,86 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		//System.out.println("hello");
+		//ArrayListBasic();
+		//ArrayListModivy();
+		//ArrayToArrayList();
+		SearchSortArrayList();
+	}
+	
+	private static void ArrayListBasic() {
 		List<String> list = new ArrayList<String>();
 		list.add("one");
 		list.add("two");
 		list.add("three");
 		System.out.println(list);
+		
+		Iterator<String> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			String str = iterator.next();
+			System.out.println(str);
+		}
+		String item = list.get(1);
+		System.out.printf("Item index 1: %s\n", item);
+		
+		for(String str : list) {
+			System.out.println(str);
+		}
 	}
+	
+	private static void ArrayListModivy() {
+		List<StringBuilder> lst = new ArrayList<StringBuilder>();
+		lst.add(new StringBuilder("alpha"));
+		lst.add(new StringBuilder("beta"));
+		lst.add(new StringBuilder("charlie"));
+		System.out.println(lst); // [alpha, beta, charlie]
 
+		for (StringBuilder sb : lst) {
+		sb.append("88"); // can modify "mutable" objects
+		}
+		System.out.println(lst); // [alpha88, beta88, charlie88]
+	}
+	
+	public static void ArrayToArrayList() {
+		String[] strs = { "alpha", "beta", "charlie" };
+		System.out.println(Arrays.toString(strs)); // [alpha, beta, charlie]
+
+		List<String> lst = Arrays.asList(strs);
+		System.out.println(lst); // [alpha, beta, charlie]
+
+		// Changes in array or list write thru
+		strs[0] += "88";
+		lst.set(2, lst.get(2) + "99");
+		System.out.println(Arrays.toString(strs)); // [alpha88, beta, charlie99]
+		System.out.println(lst); // [alpha88, beta, charlie99]
+
+		// Initialize a list using an array
+		List<Integer> lstInt = Arrays.asList(22, 44, 11, 33);
+		System.out.println(lstInt); // [22, 44, 11, 33]
+	}
+	
+	public static void SearchSortArrayList() {
+	     // Sort and search an "array" of Strings
+	     String[] array = {"Hello", "hello", "Hi", "HI"};
+
+	     // Use the Comparable defined in the String class
+	     Arrays.sort(array);
+	     System.out.println(Arrays.toString(array));  // [HI, Hello, Hi, hello]
+
+	     // Try binary search - the array must be sorted
+	     System.out.println(Arrays.binarySearch(array, "Hello")); // 1
+	     System.out.println(Arrays.binarySearch(array, "HELLO")); // -1 (insertion at index 0)
+
+	     // Sort and search a "List" of Integers
+	     List<Integer> lst = new ArrayList<Integer>();
+	     lst.add(22);  // auto-box
+	     lst.add(11);
+	     lst.add(44);
+	     lst.add(33);
+	     Collections.sort(lst);    // Use the Comparable of Integer class
+	     System.out.println(lst);  // [11, 22, 33, 44]
+	     System.out.println(Collections.binarySearch(lst, 22)); // 1
+	     System.out.println(Collections.binarySearch(lst, 35)); // -4 (insertion at index 3)
+	}
+	
 }
