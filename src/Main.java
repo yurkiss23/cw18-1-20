dafb5e198d6f081599459d1237ff355ca11fb50e
Anonymous <user@rivne.itstep.org>
2020-01-18 09:50:22 +0200

    begin

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		List<String> list = new ArrayList<String>();
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		System.out.println(list);
+	}
+
+}
